831e6e8ed9e5776adb67dccb83ca819ec7c4743e
Shivesh Pathak <sapatha2@illinois.edu>
2017-11-14 11:56:07 -0600

    Initial commit

---
+++ undoped/gencrystal.py
@@ -0,0 +1,35 @@
+import pymatgen as mg
+from Crystal import CrystalWriter
+from pymatgen.io.cif import CifParser
+
+'''
+#Write CIF file
+lattice=mg.Lattice.tetragonal(7.8558,3.4338)
+structure=mg.Structure(lattice,["Cu","Cu","Cu","Cu",
+"O","O","O","O","O","O","O","O",
+"Sr","Sr","Sr","Sr"],
+[[0.0,0.0,0.0],[0.5,0.0,0.0],[0.0,0.5,0.0],[0.5,0.5,0.0],
+[0.25,0.0,0.0],[0.75,0.0,0.0],[0.25,0.5,0.0],[0.75,0.5,0.0],[0.0,0.25,0.0],[0.0,0.75,0.0],[0.5,0.25,0],[0.5,0.75,0.0],
+[0.25,0.25,0.5],[0.25,0.75,0.5],[0.75,0.25,0.5],[0.75,0.75,0.5]])
+structure.to(filename="SrCuO2_221sc.cif")
+'''
+
+#Read CIF file and write output
+write=CrystalWriter()
+write.xml_name='../../autogenv2/BFD_Library.xml'
+fname="SrCuO2_221sc.cif"
+myfile=open(fname,"r")
+write.set_struct_fromcif(myfile.read(),primitive=False)
+write.write_crys_input("crys_pbe0.in")
+
+#Need to add
+'''
+SUPERCON (vs SUPERCEL)
+MODISYMM
+Make sure basis cutoff is okay
+SHRINK 
+MAXCYCL
+ATOMSPIN
+'''
+
+
